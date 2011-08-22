27dcf89150d82fc5bba61e17f59302307a31f88f
nicolasfranck <nicolas.franck@ugent.be>
2011-08-22 17:05:56 +0200

    fix urlvars

--- htdocs/js/urlvars.js
+++ htdocs/js/urlvars.js
@@ -1,10 +1,12 @@
 function getUrlVars(){
-    var vars = [], hash;
-    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
-    for(var i = 0; i < hashes.length; i++)
-    {
-        hash = hashes[i].split('=');
-        vars[hash[0]] = hash[1];
-    }
-    return vars;
+	var vars = [], hash;
+	var index = location.search.indexOf('?');
+	if(index != -1){
+		var hashes = location.search.substr(index+1).split('&');
+		for(var i = 0; i < hashes.length; i++){
+			hash = hashes[i].split('=');
+			vars[hash[0]] = hash[1];
+		}
+	}
+	return vars;
 }
